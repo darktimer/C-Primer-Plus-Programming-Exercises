f220dc0c5f79c2b6a72c0aae7f3347801c5f8fb5
darktimer <darktimer.yz@outlook.com>
2025-11-25 17:08:27 +0800

    2025-11-24

--- chapter 4/test.c
+++ chapter 4/test.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+#define Q "His Hamlet was funny without being vulgar."
 
 int main(void){
-	char ch[10];
-	
-	scanf("%s",ch);
-	printf("%s",ch);
+	printf("%s\nhas %dcharacters.\n", Q, strlen(Q));
+	printf("Is %2.2e the same as %2.2f?\n", 1201.0, 1201.0);
 	
 	return 0;
 }
+
