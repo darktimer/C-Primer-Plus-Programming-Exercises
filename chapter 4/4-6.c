f2a8af46bdc98437b953245a88c4721a7ca8523b
darktimer <darktimer.yz@outlook.com>
2025-12-26 10:20:05 +0800

    Update 4-6.c

--- chapter 4/4-6.c
+++ chapter 4/4-6.c
@@ -2,5 +2,16 @@
 #include <string.h>
 
 int main(void){
-	char fname[10]='',sname[10]='';
+	char fname[20]="",sname[20]="";
+
+	printf("input second name:");
+	scanf("%s",sname);
+	printf("input first name:");
+	scanf("%s",fname);
+	printf("name confim:\n%s %s\n",sname,fname);
+	printf("%*d %*d\n",strlen(sname),strlen(sname),strlen(fname),strlen(fname));
+	printf("%s %s\n",sname,fname);
+	printf("%-*d %-*d",strlen(sname),strlen(sname),strlen(fname),strlen(fname));
+
+	return 0;
 }
